5cc810d7b01894cc8651e0147eb5dbba6a2fac9d
AlexS1302 <135150527+AlexS1302@users.noreply.github.com>
2025-01-27 17:40:09 +0000

    Create initial layout
    -Create the basic layout structure for the page
    -Implement an initial version of the sidebar with placeholder links
    -Implement initial styling for the header and sidebar

---
+++ js-code.js
@@ -0,0 +1,17 @@
+const myLibrary = [];
+
+function Book(title, author, pages, isRead) {
+    // constructor
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+    
+}
+
+function addBookToLibrary(title, author, pages, isRead) {
+    const newBook = new Book(title, author, pages, isRead)
+    myLibrary.push(newBook);
+    
+}
+
