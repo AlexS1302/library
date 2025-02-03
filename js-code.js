e9f02225e4610338a8f8526757663d295cc434ca
AlexS1302 <135150527+AlexS1302@users.noreply.github.com>
2025-02-03 14:22:25 +0000

    Add placeholder books to myLibrary array

--- js-code.js
+++ js-code.js
@@ -1,7 +1,9 @@
+
 const myLibrary = [];
 
-function Book(title, author, pages, isRead) {
-    // constructor
+// Book constructor
+
+function Book(title, author, pages, isRead, image) {
     this.title = title;
     this.author = author;
     this.pages = pages;
@@ -15,4 +17,27 @@ function addBookToLibrary(title, author, pages, isRead, image) {
     myLibrary.push(newBook);
     
 }
+
+// Placeholder Books displayed when the page has loaded
+
+addBookToLibrary(
+    "Pride and Prejudice",
+    "Jane Austen",
+    334,
+    false,
+    "./photos/pride-and-prejudice.jpg");
+
+addBookToLibrary(
+    "To Kill a Mockingbird",
+    "Harper Lee",
+    323,
+    false,
+    "./photos/to-kill-a-mockingbird.jpg");
+
+addBookToLibrary(
+    "Harry Potter and the Order of the Phoenix",
+    "J. K. Rowling",
+    912,
+    false,
+    "./photos/harry-potter-and-the-order-of-the-phoenix.jpg");
 
