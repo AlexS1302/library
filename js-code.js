6804cbc1323404ce0ed3098603a7a582779b1f1e
AlexS1302 <135150527+AlexS1302@users.noreply.github.com>
2025-02-03 13:01:58 +0000

    Add modal section in index.html
    -Add image parameter to Book and addBookToLibrary functions
    -Update .library and .library-top styles

--- js-code.js
+++ js-code.js
@@ -6,11 +6,12 @@ function Book(title, author, pages, isRead) {
     this.author = author;
     this.pages = pages;
     this.isRead = isRead;
+    this.image = image;
     
 }
 
-function addBookToLibrary(title, author, pages, isRead) {
-    const newBook = new Book(title, author, pages, isRead)
+function addBookToLibrary(title, author, pages, isRead, image) {
+    const newBook = new Book(title, author, pages, isRead, image)
     myLibrary.push(newBook);
     
 }
