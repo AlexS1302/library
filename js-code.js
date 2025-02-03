7571783b32abd136252668f59e3dab264b19fcd9
AlexS1302 <135150527+AlexS1302@users.noreply.github.com>
2025-02-03 18:37:12 +0000

    Restructure code layout and enhance functionality
    -Restructure code layout in index.html
    -Add displayBooks function in JS file
    -Add single quotes to all url() in CSS file

--- js-code.js
+++ js-code.js
@@ -9,13 +9,11 @@ function Book(title, author, pages, isRead, image) {
     this.pages = pages;
     this.isRead = isRead;
     this.image = image;
-    
 }
 
 function addBookToLibrary(title, author, pages, isRead, image) {
     const newBook = new Book(title, author, pages, isRead, image)
     myLibrary.push(newBook);
-    
 }
 
 // Placeholder Books displayed when the page has loaded
@@ -25,19 +23,57 @@ addBookToLibrary(
     "Jane Austen",
     334,
     false,
-    "./photos/pride-and-prejudice.jpg");
+    "./photos/pride-and-prejudice.jpg"
+);
 
 addBookToLibrary(
     "To Kill a Mockingbird",
     "Harper Lee",
     323,
     false,
-    "./photos/to-kill-a-mockingbird.jpg");
+    "./photos/to-kill-a-mockingbird.jpg"
+);
 
 addBookToLibrary(
     "Harry Potter and the Order of the Phoenix",
     "J. K. Rowling",
     912,
     false,
-    "./photos/harry-potter-and-the-order-of-the-phoenix.jpg");
+    "./photos/harry-potter-and-the-order-of-the-phoenix.jpg"
+);
 
+
+// Displaying the books on the page
+
+function displayBooks(books) {
+    books.forEach((book) => {
+        const bookList = document.querySelector(".books");
+
+        const bookElement = document.createElement("div");
+        bookElement.classList.add("book");
+
+        const bookImage = document.createElement("div");
+        bookImage.classList.add("book-image");
+        bookImage.style.backgroundImage = `url('${book.image}')`;
+
+        const bookPages = document.createElement("div");
+        bookPages.classList.add("display-book-pages");
+        bookPages.textContent = `${book.pages} Pages`;
+
+        bookImage.appendChild(bookPages);
+        bookElement.appendChild(bookImage);
+        bookList.appendChild(bookElement);
+
+        const bookDetails = document.createElement("div");
+
+
+
+
+
+
+
+
+    })
+}
+
+displayBooks(myLibrary);
